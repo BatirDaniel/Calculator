afe2251abc0bede0d1e14166c5bc85b02503249e
BatirDaniel <daniel.batir2003@gmail.com>
2023-05-24 15:31:22 +0300

    Added style for calculator and functionality to display values in input

---
+++ Calculator/Calculator/wwwroot/js/main.js
@@ -0,0 +1,34 @@
+﻿const appendExpression = (value) => {
+    document.getElementById("expressionInput").value += value;
+}
+
+const clearExpression = () => {
+    document.getElementById("expressionInput").value = "";
+}
+
+const evaluateExpression = () => {
+    var expression = document.getElementById("expressionInput").value;
+
+    expression.innerHTML = calculateExpression(expression.value.toString());
+
+    //var xhr = new XMLHttpRequest();
+    //xhr.open("POST", "/Calculator/EvaluateExpression", true);
+    //xhr.setRequestHeader("Content-Type", "application/json");
+    //xhr.onreadystatechange = function () {
+    //    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    //        var result = JSON.parse(xhr.responseText);
+    //        document.getElementById("expressionInput").value = result;
+    //    }
+    //};
+    //xhr.send(JSON.stringify({ expression: expression }));
+}
+
+const calculateExpression = (expr) => {
+    var total = 0;
+    expr = expr.replace(/\s/g, '').match(/[+\-]?([0-9\.\s]+)/g) || [];
+
+    while (expr.length) {
+        total += parseFloat(expr.shift());
+    }
+    return total;
+}
