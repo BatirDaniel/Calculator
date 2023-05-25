64149924e7eafd6c1cbde4c65376b53c1f656217
BatirDaniel <daniel.batir2003@gmail.com>
2023-05-25 17:15:06 +0300

    Added algorithm for pharanteses and some files were deleted

--- Calculator/Calculator/wwwroot/js/site.js
+++ Calculator/Calculator/wwwroot/js/site.js
@@ -1,4 +1,75 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const appendExpression = (value) => {
+    const expression = document.getElementById("expressionInput");
 
-// Write your JavaScript code.
+    var regex = /[+\-*/.]/;
+    const lastElement = expression.value[expression.value.length - 1];
+
+    if (regex.test(lastElement) && regex.test(value)) {
+        expression.value = expression.value.slice(0, -1);
+        expression.value += value;
+    }
+    else {
+        regexNumber = /[0-9]/g;
+        if (regexNumber.test(lastElement) && value === "(") {
+            expression.value += "*" + value;
+        }
+        else if (lastElement === ")" && regexNumber.test(value)) {
+            expression.value += "*" + value;
+        }
+        else if (lastElement === "(" && regex.test(value)) {
+            return;
+        } else if (regex.test(lastElement) && value === ")") {
+            expression.value = expression.value.slice(0, -1);
+            expression.value += value;
+        } else {
+            expression.value += value;
+        }
+    }
+}
+
+const clearExpression = () => {
+    document.getElementById("expressionInput").value = "";
+}
+
+const evaluateExpression = () => {
+    const expression = document.getElementById("expressionInput").value;
+
+    expression.value = calculateExpression(expression);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/Calculator/EvaluateExpression", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                const result = JSON.parse(xhr.responseText);
+                document.getElementById("expressionInput").value = result;
+            } else {
+                console.error(xhr.statusText);
+                const result = JSON.parse(xhr.responseText);
+                document.getElementById("expressionInput").value = result.error;
+            }
+        }
+    };
+    xhr.onerror = function () {
+        console.error(xhr.statusText);
+    };
+    xhr.send(JSON.stringify({ expression: expression }));
+}
+
+const calculateExpression = (expr) => {
+    var total = 0;
+
+    expr = expr.replace(/\s/g, '').match(/[+\-]?([0-9\.\s]+)/g) || [];
+
+    while (expr.length) {
+        total += parseFloat(expr.shift());
+    }
+
+    return total;
+}
+
+const clearLastElement = () => {
+    const expression = document.getElementById("expressionInput");
+    expression.value = expression.value.slice(0, -1);
+}
